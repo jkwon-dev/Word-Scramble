0631fccd2d85c90fb6ddf4fc5c17318d95f5ea59
jkwon-dev <kwonejk@gmail.com>
2023-11-30 14:50:40 +0900

    Word Scramble Tutorial 4/6 - Adding to a list of words

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -8,20 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     var body: some View {
-        Text("Hello")
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section {
+                    ForEach(useWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit {
+                addNewWord()
+            }
+        }
     }
-    
-    func testStrings() {
-        let word = "swift"
-        let checker = UITextChecker()
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
-        let allGood = misspelledRange.location == NSNotFound
-        
+        // extra validation to come
+        withAnimation {
+            useWords.insert(answer, at:0)
+        }
+        newWord = ""
     }
 }
 
