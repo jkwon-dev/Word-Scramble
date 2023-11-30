a06747295297c9948ad84343dcadf5ca38d78b50
jkwon-dev <kwonejk@gmail.com>
2023-11-30 15:10:32 +0900

    Word Scramble Tutorial 5/6 - Running code when our app launches

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -32,6 +32,7 @@ struct ContentView: View {
             .onSubmit {
                 addNewWord()
             }
+            .onAppear(perform: startGame)
         }
     }
     func addNewWord() {
@@ -43,6 +44,19 @@ struct ContentView: View {
         }
         newWord = ""
     }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
 }
 
 #Preview {
