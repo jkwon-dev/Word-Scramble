4e4fc34a216349627091df6f8cddd93f47472da2
jkwon-dev <kwonejk@gmail.com>
2023-11-30 13:32:29 +0900

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by kwon eunji on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
