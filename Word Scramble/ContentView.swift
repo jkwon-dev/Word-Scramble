3ac03c6ad385abbb6315fa3715f03ee05d38ae97
jkwon-dev <kwonejk@gmail.com>
2023-11-30 14:26:49 +0900

    Word Scramble Tutorial 3/6 - Working with strings

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -11,15 +11,17 @@ struct ContentView: View {
     
     var body: some View {
         Text("Hello")
-        
     }
     
-    func testBunles() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string
-            }
-        }
+    func testStrings() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+        
     }
 }
 
